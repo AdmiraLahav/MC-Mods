029a353b1f218f034fa48bf25adf79c281cce4aa
AdmiraLahav <lavhrmn@gmail.com>
2026-02-22 18:42:08 +0200

    2

---
+++ src/main/java/lav/learning/LavlearningClient.java
@@ -0,0 +1,11 @@
+package lav.learning;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LavlearningClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

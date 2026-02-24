a7847cea4dfb7a425525ea30e9d5bf450f5ff613
LH <113438937+AdmiraLahav@users.noreply.github.com>
2026-02-24 18:01:57 +0200

    Refactor item registration to set registry keys before construction

--- src/main/java/lav/learning/item/ModItems.java
+++ src/main/java/lav/learning/item/ModItems.java
@@ -6,25 +6,31 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
 
     //--create the custom modded Item "CUMTOM_ITEM_NAME" with the ingame name of it lowercase
-    public static final Item CUSTOM_ITEM_NAME = RegisterItem("custom_item_name", new Item(new Item.Settings()));
+    public static final Item CUSTOM_ITEM_NAME = registerItem("custom_item_name");
 
-    public static Item RegisterItem(String name, Item item){
-        return Registry.register(Registries.ITEM, Identifier.of(Lavlearning.MOD_ID,name), item);
+    private static Item registerItem(String name) {
+        Identifier id = Identifier.of(Lavlearning.MOD_ID, name);
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, id);
+        Item item = new Item(new Item.Settings().registryKey(itemKey));
+
+        return Registry.register(Registries.ITEM, id, item);
     }
 
-    public static void RegisterModItems(){
+    public static void registerModItems() {
         Lavlearning.LOGGER.info("RegModItems has been called");
         Lavlearning.LOGGER.info("Registering Mod items for " + Lavlearning.MOD_ID);
 
         //--add the item to the inventory catagory
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(fabricItemGroupEntries -> {
             //--add the custom Item just made to a group
-           fabricItemGroupEntries.add(CUSTOM_ITEM_NAME);
+            fabricItemGroupEntries.add(CUSTOM_ITEM_NAME);
         });
     }
 }

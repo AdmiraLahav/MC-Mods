5c07fe79cf4a18b6e1b6ef0265e2b491d2b1f837
AdmiraLahav <lavhrmn@gmail.com>
2026-02-24 22:57:47 +0200

    it works, thank god

--- src/main/java/lav/learning/LavlearningDataGenerator.java
+++ src/main/java/lav/learning/LavlearningDataGenerator.java
@@ -1,11 +1,14 @@
 package lav.learning;
 
+import lav.learning.datagen.ModModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class LavlearningDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModModelProvider::new);
 	}
 }
